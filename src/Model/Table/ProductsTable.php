9acaf8f94b00b6e4f60d4bb3d77bbebb5dcc7965
Jamie Shin <jshi36@student.monash.edu>
2016-09-30 12:06:09 +1000

    no message

---
+++ src/Model/Table/ProductsTable.php
@@ -0,0 +1,98 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Products Model
+ *
+ * @property \Cake\ORM\Association\BelongsTo $Subcategory
+ *
+ * @method \App\Model\Entity\Product get($primaryKey, $options = [])
+ * @method \App\Model\Entity\Product newEntity($data = null, array $options = [])
+ * @method \App\Model\Entity\Product[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\Product|bool save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Product patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\Product[] patchEntities($entities, array $data, array $options = [])
+ * @method \App\Model\Entity\Product findOrCreate($search, callable $callback = null)
+ */
+class ProductsTable extends Table
+{
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->table('products');
+        $this->displayField('name');
+        $this->primaryKey(['id', 'subcategory_id']);
+
+        $this->belongsTo('Subcategory', [
+            'foreignKey' => 'subcategory_id',
+            'joinType' => 'INNER'
+        ]);
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->integer('id')
+            ->allowEmpty('id', 'create');
+
+        $validator
+            ->requirePresence('name', 'create')
+            ->notEmpty('name');
+
+        $validator
+            ->requirePresence('desc', 'create')
+            ->notEmpty('desc');
+
+        $validator
+            ->boolean('ship')
+            ->requirePresence('ship', 'create')
+            ->notEmpty('ship');
+
+        $validator
+            ->integer('qty')
+            ->allowEmpty('qty');
+
+        $validator
+            ->numeric('price')
+            ->requirePresence('price', 'create')
+            ->notEmpty('price');
+
+        $validator
+            ->allowEmpty('image');
+
+        return $validator;
+    }
+
+    /**
+     * Returns a rules checker object that will be used for validating
+     * application integrity.
+     *
+     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
+     * @return \Cake\ORM\RulesChecker
+     */
+    public function buildRules(RulesChecker $rules)
+    {
+        $rules->add($rules->existsIn(['subcategory_id'], 'Subcategory'));
+
+        return $rules;
+    }
+}
